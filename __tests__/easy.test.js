0536df796a5d5a9c96cb95206016b7aaec0f2467
CarlieMartece <carliemartece@gmail.com>
2023-05-20 19:15:05 +0100

    complete longestPassword function with relevant tests

--- __tests__/easy.test.js
+++ __tests__/easy.test.js
@@ -2,10 +2,37 @@ const {
     longestPassword
 } = require("../easy");
 
+// it has to contain only alphanumerical characters (a−z, A−Z, 0−9);
+// there should be an even number of letters;
+// there should be an odd number of digits.
 describe("LongestPassword", () => {
     test("Function returns a number", () => {
-        const actual = longestPassword();
+        const actual = longestPassword("t3st");
         const expected = -1;
         expect(actual).toBe(expected);
-    })
+    });
+    test("Function checks the only word in single-word array for password validity", () => {
+        let actual = longestPassword("Nook314");
+        let expected = 7;
+        expect(actual).toBe(expected);
+        actual = longestPassword("Nooka415");
+        expected = -1;
+        expect(actual).toBe(expected);
+    });
+    test("Function checks all words in multi-word array and returns longest valid password", () => {
+        let actual = longestPassword("T3sting N0oka");
+        let expected = 7;
+        expect(actual).toBe(expected);
+        actual = longestPassword("You 3re g1ven a str1ng S Ccons1sting of N characters.");
+        expected = 11;
+        expect(actual).toBe(expected);
+    });
+    test("Function accepts words with no letters", () => {
+        let actual = longestPassword("0")
+        let expected = 1;
+        expect(actual).toBe(expected);
+        actual = longestPassword("3141 592 653589793");
+        expected = 9;
+        expect(actual).toBe(expected);
+    });
 });
--- easy.js
+++ easy.js
@@ -1,4 +1,19 @@
 exports.longestPassword = (S) => {
     let longest = -1;
+    let wordArr = S.split(" ");
+    if (!wordArr) wordArr = [S];
+    wordArr.forEach((word) => {
+        if (/^[a-zA-Z0-9]*$/.test(word) && word.length > longest) {
+            let numbers = word.match(/[0-9]/g);
+            let letters = word.match(/[a-zA-Z]/g);
+            if (numbers && numbers.length % 2 === 1) {
+                if (!letters) {
+                    longest = word.length;
+                } else if (letters.length % 2 === 0) {
+                    longest = word.length;
+                };
+            };
+        };
+    });
     return longest;
-}
+};
